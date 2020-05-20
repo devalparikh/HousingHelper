d822ebd0623af354cece9356e8d55cbb4a7556fe
Deval Parikh <devalpp@gmail.com>
2020-05-20 19:26:09 -0400

    readme updates

--- src/App.js
+++ src/App.js
@@ -77,7 +77,7 @@ export default function App() {
           {/* <Button color="inherit">Login</Button> */}
         </Toolbar>
       </AppBar>
-      <LocationPicker />
+      {/* <LocationPicker /> */}
 
     </div>
   );
