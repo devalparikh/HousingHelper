34c37e7558c3ef4b8e079dd06799c09fddaa30fe
Deval Parikh <devalpp@gmail.com>
2020-07-15 23:23:25 -0400

    heroku

--- backend/server.js
+++ backend/server.js
@@ -41,14 +41,14 @@ app.use('/auth', authRouter);
 
 // app.get('/', (req, res) => res.send('hello'));
 
-// app.use(express.static('./../build'));
+app.use(express.static('./../build'));
     app.get('/*', (req, res) => {
         res.sendFile(path.join(__dirname, './../build', 'index.html')); // Relative Path
     })
 
 if(process.env.NODE_ENV === 'production') {
     // Serve React Application
-    // app.use(express.static('./../build'));
+    app.use(express.static('./../build'));
     app.get('/*', (req, res) => {
         res.sendFile(path.join(__dirname, './../build', 'index.html')); // Relative Path
     })
