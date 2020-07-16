49c54bd08d53b28bfeaac6f2917523e32154fe4b
Deval Parikh <devalpp@gmail.com>
2020-07-15 23:58:18 -0400

    proxy url update

--- src/constant.js
+++ src/constant.js
@@ -1,2 +1,2 @@
-export const apiURL = "https://h-helper.herokuapp.com/"; // Deployed
+export const apiURL = "https://h-helper.herokuapp.com"; // Deployed
 // export const apiURL = "http://localhost:5000"; // Local
