10a82ea9e4bc2866765e191fa265ef65de5fb572
Deval Parikh <devalpp@gmail.com>
2020-07-15 23:40:46 -0400

    heroku

--- backend/server.js
+++ backend/server.js
@@ -41,7 +41,7 @@ app.use('/auth', authRouter);
 
 // app.get('/', (req, res) => res.send('hello'));
 
-const root = require('path').join(__dirname, 'build')
+const root = require('path').join(__dirname, './../build')
     app.use(express.static(root));
     app.get("*", (req, res) => {
         res.sendFile('index.html', { root });
