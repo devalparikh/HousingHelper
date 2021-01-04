f7f2299a436151e1239d88440524744942726361
Deval Parikh <devalpp@gmail.com>
2021-01-03 22:47:43 -0500

    started full text search

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -8,32 +8,74 @@ require('dotenv').config();
 // all endpoints after /posts
 // example http://localhost:5000/posts
 
-// @route Get /
+// @route Get /posts/
 // @desc Gets all Posts
 // @access Public
 router.route('/').get((req, res) => {
   Post.find().sort([['createdAt', -1]]) // find() returns promise
     .then(posts => res.json(posts)) // returns Posts
-    .catch(err => res.status(400).json({msg: err})); // catches errors and returns err
+    .catch(err => res.status(400).json({ msg: err })); // catches errors and returns err
 });
 
-// @route GET /post/:id
+// @route GET /posts/post/:id
 // @desc Get a Post by id
 // @access Public
 router.route('/post/:id').get((req, res) => {
   Post.findById(req.params.id) // find() returns promise
     .then(Post => res.json(Post)) // returns Posts
-    .catch(err => res.status(400).json({msg: err})); // catches errors and returns err
+    .catch(err => res.status(400).json({ msg: err })); // catches errors and returns err
 });
 
 // @route GET /posts/byuserid/:id
 // @desc Get a Posts by their username
 // @access Public
 router.route('/byuserid/:user_id').get((req, res) => {
-    Post.find({user_id: (req.params.user_id)}).sort([['createdAt', -1]])
+  Post.find({ user_id: (req.params.user_id) }).sort([['createdAt', -1]])
     .then(post => res.json(post))
     .catch(err => res.status(400).json('Error: ' + err));
-  });
+});
+
+// @route POST /posts/post/search
+// @desc Search posts
+// @access Public
+router.route('/search').post(async(req, res) => {
+
+  const moreInfo_posts = await Post.aggregate([
+    {
+      $search: {
+        "text": {
+          'query': req.body.search_text,
+          'path': 'moreInfo'
+        }
+      }
+    },
+    {
+      $limit: 5
+    }
+  ]);
+
+  const username_posts = await Post.aggregate([
+    {
+      $search: {
+        "text": {
+          'query': req.body.search_text,
+          'path': 'username'
+        }
+      }
+    },
+    {
+      $limit: 5
+    }
+  ]);
+
+  const searchResults = {
+    moreInfoPosts: moreInfo_posts,
+    usernamePosts: username_posts
+  }
+
+  res.json(searchResults);
+
+});
 
 // @route POST /posts/post/create
 // @desc Creates new post
@@ -50,7 +92,7 @@ router.route('/byuserid/:user_id').get((req, res) => {
 //     rentPrice: req.body.rentPrice,
 //     moreInfo: req.body.moreInfo,
 //   })
-  
+
 //   User.findById(newPost.user_id)
 //     .then((user) => {
 //       if(user) {
@@ -64,9 +106,8 @@ router.route('/byuserid/:user_id').get((req, res) => {
 //     })
 //     // user_id not found in users
 //     .catch(err => res.status(400).json('Error: ' + err));
-  
-//   });
 
+//   });
 
 
 module.exports = router;
